8b1bb4746a663f666e72ca4dfc66795f9bd232bc
brallex91 <allex91@live.se>
2023-03-03 22:40:41 +0100

    Add MainMenu

---
+++ Goblin King/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public string levelToLoad;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+
+        Debug.Log("Quitting Game.");
+    }
+}
